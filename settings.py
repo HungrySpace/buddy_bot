c700a38e7124f8c36ecd6afb0aebf579a901eb27
HungrySpace <46231064+HungrySpace@users.noreply.github.com>
2021-08-22 20:43:57 +0300

    Update settings.py
    Token

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv, find_dotenv
 # Loading .env variables
 load_dotenv(find_dotenv())
 
-TELEGRAM_TOKEN = "1764514462:AAEEjdaLB_2GpZcDSKh7p7RtgiZ-7A5sn3Y"# os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 if TELEGRAM_TOKEN is None:
     raise Exception("Please setup the .env variable TELEGRAM_TOKEN.")
